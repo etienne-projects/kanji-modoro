7da95cdaeaa91dfccd61f2decee3f55d992bad3d
Etienne <phone.mfe@gmail.com>
2019-08-22 11:16:40 -0400

    added N4 G1-G3

--- assets/data/n4.js
+++ assets/data/n4.js
@@ -3,193 +3,193 @@ const N4 = {
             [
                 {   
                     "kanji" : "不",
-                    "onyomi" : "ふ",
+                    "onyomi" : "フ、ブ",
                     "kunyomi" : "",
-                    "definition" : "negative, non-",
+                    "definition" : "negative, non-, bad, ugly, clumsy",
                     "examples" : ["不足 ふそく insufficiency, shortage (na-adj,n)","不安 ふあん anxiety, uneasiness (na-adj,n)","不全 ふぜん partial, incomplete (na-adj,n)"]
                 },{
                     "kanji" : "世",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "セイ、セ、ソウ",
+                    "kunyomi" : "よ",
+                    "definition" : "world, generation, society, public",
+                    "examples" : ["世界 せかい world, society (n)","世話 せわ looking after, help (n, p-suru)","世話人 せわにん manager, sponsor, agent (n)"]
                 },{
                     "kanji" : "主",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "シュ、 ス、 シュウ",
+                    "kunyomi" : "ぬし、 おも、 あるじ",
+                    "definition" : "lord, chief, master, main thing, principal",
+                    "examples" : ["主人 しゅじん master, landlord, husband, employer (n)","主食 しゅしょく staple food (n)","主語 しゅご subject (n, gram)"]
                 },{   
                     "kanji" : "乗",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "ジョウ、 ショウ",
+                    "kunyomi" : "の.る、 -の.り、 の.せる",
+                    "definition" : "ride, power, multiplication, record, counter for vehicles, board, mount, join",
+                    "examples" : ["乗る のる to ride, to board, to get on (u-v)","乗せる のせる to give a ride, to let (one) take part (u-v)","乗り場 のりば place for boarding vehicles (n)"]
                 },{
                     "kanji" : "事",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "ジ、 ズ",
+                    "kunyomi" : "こと、 つか.う、 つか.える",
+                    "definition" : "matter, thing, fact, business, reason, possibly",
+                    "examples" : ["仕事 しごと work, occupation (no-adj, n)","火事 かじ fire (n)","行事 ぎょうじ event, function (n)"]
                 },{
                     "kanji" : "京",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "キョウ、 ケイ、 キン",
+                    "kunyomi" : "みやこ",
+                    "definition" : "capital",
+                    "examples" : ["東京 とうきょう Tokyo - Capital of Japan (place)","京都 きょうと kyoto (place)","上京 じょうきょう heading to the capital (n, p-suru)"]
                 },{   
                     "kanji" : "仕",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "シ、 ジ",
+                    "kunyomi" : "つか.える",
+                    "definition" : "attend, doing, official, serve",
+                    "examples" : ["仕事 しごと work, occupation (no-adj, n)","仕事場 しごとば place where one works (n)","仕上げる しあげる to finish up, to complete (ru-v, trans-verb)"]
                 },{   
                     "kanji" : "代",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "ダイ、 タイ",
+                    "kunyomi" : "か.わる、 かわ.る、 かわ.り、 か.わり、 -がわ.り、 -が.わり、 か.える、 よ、 しろ",
+                    "definition" : "substitute, change, convert, replace, period, age, counter for decades of ages, eras, etc., generation, charge, rate, fee",
+                    "examples" : ["代わる かわる to take the place of (u-v, intrans-verb)","時代 じだい period, era (n)","代金 だいきん price, payment, cost, the bill (n)"]
                 },{   
                     "kanji" : "以",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "イ",
+                    "kunyomi" : "もっ.て",
+                    "definition" : "by means of, because, in view of, compared with",
+                    "examples" : ["以上 いじょう more than, over, this is all (n)","以下 いか less than, below, under (n)","以内 いない within, inside of (n, n-suf)"]
                 },{   
                     "kanji" : "低",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "onyomi" : "テイ",
+                    "kunyomi" : "ひく.い、 ひく.める、 ひく.まる",
+                    "definition" : "lower, short, humble",
+                    "examples" : ["低音 ていおん low tone, bass (n)","低い ひくい short, low, humble (adj)","高低 こうてい high and low, rise and fall (n)"]
                 }
             ],
 
     "Group 02" :
             [
                 {   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "住",
+                    "onyomi" : "ジュウ、 ヂュウ、 チュウ",
+                    "kunyomi" : "す.む、 す.まう、 -ず.まい",
+                    "definition" : "dwell, reside, live, inhabit",
+                    "examples" : ["住む すむ to reside, to live in (u-v)","住所 じゅうしょ address (eg house), residence (n)","安住 あんじゅう living peaceably (n, p-suru)"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "体",
+                    "onyomi" : "タイ、 テイ",
+                    "kunyomi" : "からだ、 かたち",
+                    "definition" : "body, substance, object, reality, counter for images",
+                    "examples" : ["本体 ほんたい substance, real form (n)","死体 したい corpse (n)","正体 しょうたい natural shape, true character (n)"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "作",
+                    "onyomi" : "サク、 サ",
+                    "kunyomi" : "つく.る、 つく.り、 -づく.り",
+                    "definition" : "make, production, prepare, build",
+                    "examples" : ["作家 さっか author, writer (n)","工作 こうさく work, handicraft (n)","作り話 つくりはなし fiction, made-up story, myth (n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "使",
+                    "onyomi" : "シ",
+                    "kunyomi" : "つか.う、 つか.い、 -つか.い、 -づか.い",
+                    "definition" : "use, send on a mission, order, messenger, envoy, ambassador, cause",
+                    "examples" : ["使い方 つかいかた way to use something (n)","天使 てんし angel (n)","使者 ししゃ messenger (n)"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "便",
+                    "onyomi" : "ベン、 ビン",
+                    "kunyomi" : "たよ.り",
+                    "definition" : "convenience, facility, excrement, feces, letter, chance",
+                    "examples" : ["便 びん mail, post (n)","不便 ふべん inconvenience (na-adj)","便りがないのはよい便り たよりがないのはよいたより no news is good news"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "借",
+                    "onyomi" : "シャク",
+                    "kunyomi" : "か.りる",
+                    "definition" : "borrow, rent",
+                    "examples" : ["借りる かりる to borrow (ru-v)","借金 しゃっきん debt, loan, liabilities (n)","借り物 かりもの borrowed thing (n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "動",
+                    "onyomi" : "ドウ",
+                    "kunyomi" : "はたら.く",
+                    "definition" : "work, (kokuji)",
+                    "examples" : ["働く はたらく to work, to labour (u-v)","働き者 はたらきもの hardworker, hard working person (n)","働き口 はたらきぐち position, opening (n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "元",
+                    "onyomi" : "ゲン、 ガン",
+                    "kunyomi" : "もと",
+                    "definition" : "beginning, former time, origin",
+                    "examples" : ["元気 げんき health, spirit (na-adj)","元々 もともと originally, by nature, from the start (adv, no-adj)","地元 じもと local (n)"]
+                },{ 
+                    "kanji" : "光",  
+                    "onyomi" : "コウ",
+                    "kunyomi" : "ひか.る、 ひかり",
+                    "definition" : "ray, light",
+                    "examples" : ["電光 でんこう lightning (n)","月光 げっこう moonlight (n)","日光 にっこう sunlight, Nikko (place name) (n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
-                },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "兄",
+                    "onyomi" : "ケイ、 キョウ",
+                    "kunyomi" : "あに",
+                    "definition" : "elder brother, big brother",
+                    "examples" : ["お兄さん おにいさん (someone else's) elder brother (n)","兄 あに (your own) elder brother (n)","父兄 ふけい guardians, parents and older brothers (n)"]
                 }
             ],
     "Group 03" : 
             [
                 {   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "写",
+                    "onyomi" : "うつ.す、 うつ.る、 うつ-、 うつ.し",
+                    "kunyomi" : "シャ、 ジャ",
+                    "definition" : "copy, be photographed, describe",
+                    "examples" : ["写す うつす to copy, to duplicate (u-v)","写真 しゃしん photograph (n)","生き写し いきうつし lifelike, exact resemblance (n)"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "冬",
+                    "onyomi" : "トウ",
+                    "kunyomi" : "ふゆ",
+                    "definition" : "winter",
+                    "examples" : ["冬 ふゆ winter (n)","冬物 ふゆもの winter clothing (n)","冬日 ふゆび the winter sun (n)"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "切",
+                    "onyomi" : "き.る、 -き.る、 き.り、 -き.り、 -ぎ.り、 き.れる、 -き.れる、 き.れ、 -き.れ、 -ぎ.れ",
+                    "kunyomi" : "セツ、 サイ",
+                    "definition" : "cut, cutoff, be sharp",
+                    "examples" : ["大切 たいせつ important, valuable (na-adj, n)","切手 きって postage stamp (n)","親切 しんせつ kind, gentle (na-adj, n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "別",
+                    "onyomi" : "ベツ",
+                    "kunyomi" : "わか.れる、 わ.ける",
+                    "definition" : "separate, branch off, diverge, fork, another, extra, specially",
+                    "examples" : ["別 べつ another, particular (na-adj, n)","特別 とくべつ special (na-adj, n)","別名 べつめい alias, pseudonym (n)"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "力",
+                    "onyomi" : "リョク、 リキ、 リイ",
+                    "kunyomi" : "ちから",
+                    "definition" : "power, strength, strong, strain, bear up, exert",
+                    "examples" : ["電力 でんりょく electric power (n)","全力 ぜんりょく all one's power","水力 すいりょく hydraulic power (n)"]
                 },{
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "勉",
+                    "onyomi" : "ベン",
+                    "kunyomi" : "つと.める",
+                    "definition" : "exertion, endeavour, encourage, strive, make effort, diligent",
+                    "examples" : ["勉強 べんきょう study, diligence (n, p-suru)","勉学 べんがく study, pursuit of knowledge (n)","不勉強 ふべんきょう idleness, lack of application (n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "動",
+                    "onyomi" : "ドウ",
+                    "kunyomi" : "うご.く、 うご.かす",
+                    "definition" : "move, motion, change, confusion, shift, shake",
+                    "examples" : ["動物 どうぶつ animal (n)","自動 じどう automatic, self-motion (n)","手動 しゅどう manual (n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
+                    "kanji" : "区",
+                    "onyomi" : "ク、 オウ、 コウ",
                     "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "definition" : "ward, district",
+                    "examples" : ["区 く ward, district, section (n)","区別 くべつ distinction, differentiation, classification (n, p-suru)","区切る くぎる to punctate, to cut off, to put an end to (u-v, trans-verb)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "医",
+                    "onyomi" : "イ",
+                    "kunyomi" : "い.やす、 い.する、 くすし",
+                    "definition" : "doctor, medicine",
+                    "examples" : ["医者 いしゃ doctor (medical) (n)","医学 いがく medical science, medicine (n)","医大 いだい medical university, medical school (n)"]
                 },{   
-                    "kanji" : "",
-                    "onyomi" : "",
-                    "kunyomi" : "",
-                    "definition" : "",
-                    "examples" : ["","",""]
+                    "kanji" : "去",
+                    "onyomi" : "キョ、 コ",
+                    "kunyomi" : "さ.る、 -さ.る",
+                    "definition" : "gone, past, quit, leave, elapse, eliminate, divorce",
+                    "examples" : ["去る さる to leave, to go away (u-v)","去年 きょねん last year (expr)","連れ去る つれさる to take away (u-v)"]
                 }
             ],
 

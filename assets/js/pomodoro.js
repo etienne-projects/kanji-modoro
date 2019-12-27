38fb33a366c8cdf62111817a50d5b183b092fdda
Marj E <marj@marjorieetienne.com>
2019-12-27 17:59:29 -0500

    attempt to fix bugs with resume button clicked multiple times

--- assets/js/pomodoro.js
+++ assets/js/pomodoro.js
@@ -27,6 +27,7 @@ decreaseWork.addEventListener("click", function(){
 
 //start button
 function start(){
+    clearInterval(workSession); // Prevents crazy accumulation of calls to workCountDown...
     count = workClicks * 60;
     workSession = setInterval(workCountDown, 1000);//count down function
 }
@@ -34,7 +35,6 @@ function start(){
 //workCountDown()
 
 function workCountDown(){
-    debugger;
     let seconds = count;
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(seconds / 3600);
@@ -42,13 +42,15 @@ function workCountDown(){
     seconds -= minutes * 60; 
     document.getElementById("showtime").innerHTML = ('00' + hours).slice(-2) + "時 " + ('00' + minutes).slice(-2) + "分 " + ('00' + seconds).slice(-2)　+ "秒";
     count--;
-
+    
     if (count < 0){
         clearInterval(workSession);
         workSession = null;
         document.getElementById("showtime").innerHTML = "00時 00分 00秒";
     }
-
+    
+    console.log('inside workCountdown: ', workSession);
+    
 }
 
 
@@ -61,7 +63,12 @@ function pause(){
 
 //resume button
 function resume(){
+    //debugger;
+    console.log('inside resume: ', workSession);
+    
     workSession = setInterval(workCountDown, 1000);
+    console.log('inside resume, after: ', workSession);
+    
 }
 
 

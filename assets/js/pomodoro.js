12b9b6644eaee58ffa66c6fc0c38de85431210b6
magicandcode <magicandcode@gmail.com>
2019-12-30 20:08:54 +0100

    Refactor timer script
    More refactoring is needed, there is still duplicated code and an issue where start button won't use the time set by reset.
    
    - Use const/let instead of var
    - Add event listeners to timer buttons
    - Combine start/pause button
    - Save times in objects rather than global let variables
    - Set a max limit of 59 minutes for workDuration
    - Rename variables and functions
    - Add comments and todos
    - etc

--- assets/js/pomodoro.js
+++ assets/js/pomodoro.js
@@ -1,89 +1,130 @@
 //pomodoro
 
+const timeToString = time => time.toString().padStart(2, '0');
+
 //Set Global variables
-let workClicks = 25;
-const workDisplay = document.getElementById("set-minutes");
-let count = 0;
-let workSession;
+//let initialTimes.minutes = 25;
+const initialTimes = {
+    minutes: 25,
+    seconds: 0,
+};
+const initialDisplayTimes = () => {
+    return {
+        minutes: timeToString(initialTimes.minutes),
+        seconds: timeToString(initialTimes.seconds),
+    };
+};
+const timeRemaining = {'seconds': initialTimes.minutes * 60}; // seconds
+const currentTimes = {
+    minutes: initialTimes.minutes,
+    seconds: timeRemaining.seconds, //initialTimes.seconds,
+};
+const workSession = {'id': null,};
 
-// Set intial minutes
-document.getElementById("showtime").innerHTML = `00時 ${workClicks.toString().padStart(2, '0')}分 00秒`
+const workDurationDisplay = () => document.getElementById('set-work-duration');
+const incrementWorkDurationButton = () => document.getElementById('add-time');
+const decrementWorkDurationButton = () => document.getElementById('subtract-time');
 
+const displayMinutes = document.getElementById('display-minutes');
+const displaySeconds = document.getElementById('display-seconds');
+
+workDurationDisplay().textContent = initialDisplayTimes().minutes;
+displayMinutes.textContent = initialDisplayTimes().minutes;
+displaySeconds.textContent = initialDisplayTimes().seconds;
+
+
+// Timer Buttons
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
+const START_ICON = '&#9654;';
+const PAUSE_ICON = '&#9208;';
+const RESET_ICON = '&#8634;';
+
+//currentTimes.seconds = initialTimes.minutes * 60;
+
+startButton.addEventListener('click', () => {
+    console.log(currentTimes);
+    
+    if (workSession.id) {
+        // pauseTimer
+        // set start icon
+        startButton.innerHTML = START_ICON;
+        clearInterval(workSession.id);
+        workSession.id = null;
+    }
+    else {
+        // startTimer
+        // set pause icon
+        startButton.innerHTML = PAUSE_ICON;
+        console.log('No current session, starting timer: ', workSession
+        .id);
+        // Run timerCountDown function once before setting interval to avoid 1 s
+        timerCountDown();
+        workSession.id = setInterval(timerCountDown, 1000);
+    }
+});
+
+
+resetButton.addEventListener('click', () => {
+    if (workSession.id) {
+        clearInterval(workSession.id);
+        workSession.id = null;
+        
+    }
+
+    console.log('initial: ', initialDisplayTimes(), workSession.id);
+    currentTimes.minutes = initialTimes.minutes;
+    currentTimes.seconds = timeRemaining.seconds; // Todo: Remove possible redundant variable...
+    displayMinutes.textContent = initialDisplayTimes().minutes; //timeToString(initialTimes.minutes)//initialDisplayTimes.minutes;
+    displaySeconds.textContent = initialDisplayTimes().seconds;
+    console.log('reset...');
+});
+
+
+// Todo: Enable 
 //increase work time
-const increaseWork = document.getElementById("add-time");
-increaseWork.addEventListener("click", function(){
-    workClicks += 1;
-    workDisplay.innerHTML = workClicks;
+incrementWorkDurationButton().addEventListener("click", function(){
+    initialTimes.minutes += 1;
+    if (initialTimes.minutes > 59){ // Todo: Max and Min consts
+        initialTimes.minutes = 59;
+    };
+    workDurationDisplay().textContent = initialTimes.minutes;
 }, false);
 
 //decrease work time
-const decreaseWork = document.getElementById("subtract-time");
-decreaseWork.addEventListener("click", function(){
-    workClicks -= 1;
-    workDisplay.innerHTML = workClicks;
-    if (workClicks < 1){ // Todo: Max of 59
-        workClicks = 1;
-        workDisplay.innerHTML = workClicks;
+decrementWorkDurationButton().addEventListener("click", function(){
+    initialTimes.minutes -= 1;
+    //workDurationDisplay().innerHTML = workDuration;
+    if (initialTimes.minutes < 1){ // Todo: Max of 59
+        initialTimes.minutes = 1;
     };
+    workDurationDisplay().textContent = initialTimes.minutes;
 }, false);
 
 
-//start button
-function start(){
-    clearInterval(workSession); // Prevents crazy accumulation of calls to workCountDown...
-    count = workClicks * 60;
-    workSession = setInterval(workCountDown, 1000);//count down function
-}
 
-//workCountDown()
 
-function workCountDown(){
-    let seconds = count;
+
+// Todo: ES6, move since arrow functions are not hoisted...
+function timerCountDown(){
+    let seconds = currentTimes.seconds;
     let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(seconds / 3600);
-    seconds -= hours * 3600;
-    seconds -= minutes * 60; 
-    document.getElementById("showtime").innerHTML = ('00' + hours).slice(-2) + "時 " + ('00' + minutes).slice(-2) + "分 " + ('00' + seconds).slice(-2)　+ "秒";
-    count--;
+//    let hours = Math.floor(seconds / 3600);
+//    seconds -= hours * 3600;
+    seconds -= minutes * 60;
+    //document.getElementById("showtime").innerHTML = ('00' + hours).slice(-2) + "時 " + ('00' + minutes).slice(-2) + "分 " + ('00' + seconds).slice(-2)　+ "秒";
+    displayMinutes.textContent = timeToString(minutes);
+    displaySeconds.textContent = timeToString(seconds);
+    console.log('current: ', currentTimes.seconds, workSession.id);
+    currentTimes.seconds--;
+    console.log('current: ', currentTimes.seconds, workSession.id);
     
-    if (count < 0){
-        clearInterval(workSession);
-        workSession = null;
-        document.getElementById("showtime").innerHTML = "00時 00分 00秒";
+    if (currentTimes.seconds < 0) { // Try with less than 1
+        clearInterval(workSession.id);
+        workSession.id = null;
+        displayMinutes.textContent = timeToString(0);
+        displaySeconds.textContent = timeToString(0);
     }
     
-    console.log('inside workCountdown: ', workSession);
-    
+    console.log('inside workCountdown: ', workSession.id);
 }
-
-
-//pause button
-function pause(){
-    clearInterval(workSession);
-    workSession = null;
-}
-
-
-//resume button
-function resume(){
-    //debugger;
-    console.log('inside resume: ', workSession);
-    
-    workSession = setInterval(workCountDown, 1000);
-    console.log('inside resume, after: ', workSession);
-    
-}
-
-
-//reset button
-function reset(){
-    if(workSession){
-        clearInterval(workSession);
-        workSession = null;
-    } 
-
-    document.getElementById("showtime").innerHTML = `00時 ${workClicks.toString().padStart(2, '0')}分 00秒`;
-    document.getElementById("pause").disabled = false;
-    document.getElementById("resume").disabled = false;
-}
-
